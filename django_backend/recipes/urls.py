2a6dbbf4e6c3dfd0fd47d375e6eb7ac1f3430295
Nachiket <nmore@pdx.edu>
2024-08-20 17:57:09 -0700

    added README

--- django_backend/recipes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('recipes/', views.RecipeList.as_view(), name='recipe-list'),
-    path('recipe/<int:pk>/', views.RecipeDetail.as_view(), name='recipe-detail'),
-]
