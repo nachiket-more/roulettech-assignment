1529f7c6f28b3b3b1024ec4701e9ded387cc68dc
Nachiket <nmore@pdx.edu>
2024-08-21 17:02:08 -0700

    updated Recipe component

--- react_frontend/src/Components/Recipe/Recipe.jsx
+++ react_frontend/src/Components/Recipe/Recipe.jsx
@@ -2,24 +2,26 @@ import * as React from "react";
 import RecipeCard from '../RecipeCard/RecipeCard'
 import './Recipe.css'
 
-const Recipe = () => {
-    const [recipeData, setRecipeData] = React.useState({})
+const App = () => {
+    const [recipeData, setRecipeData] = React.useState([]);
 
     React.useEffect(() => {
-        fetch('http://recipe-django-env.eba-udmkdzi6.us-east-1.elasticbeanstalk.com/api/recipes')
-        .then(response => response.json())
-        .then(data => {
-            setRecipeData(data)
-            console.log(data)
-        })
-        .catch(error => console.log(error))
-    },[])
+        fetch('http://recipe-django-env.eba-udmkdzi6.us-east-1.elasticbeanstalk.com/api/recipes/')
+            .then(response => response.json())
+            .then(data => {
+                setRecipeData(data);
+                console.log(data);
+            })
+            .catch(error => console.log(error));
+    }, []);
 
     return (
         <div className="recipe-container">
             <div className="recipe-list">
-                {Object.keys(recipeData).length > 0 ? (
-                    <RecipeCard data={recipeData} />
+                {recipeData.length > 0 ? (
+                    recipeData.map((recipe, index) => (
+                        <RecipeCard key={index} data={recipe} />
+                    ))
                 ) : (
                     <p>No recipe data available.</p>
                 )}
@@ -28,5 +30,4 @@ const Recipe = () => {
     );
 }
 
-
-export default Recipe;
+export default App;
