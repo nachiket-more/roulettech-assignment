a2a97b3957fae9bfd31f376ab161e6e865f28001
Nachiket More <nachiketmore.more@gmail.com>
2025-06-10 11:23:00 +0530

    migrate from DynamoDB to Django ORM, update serializers and views

--- django_backend/recipes/serializers.py
+++ django_backend/recipes/serializers.py
@@ -2,6 +2,19 @@ from rest_framework import serializers
 from .models import Recipe
 
 class RecipeSerializer(serializers.ModelSerializer):
+    cuisine = serializers.SerializerMethodField()
+    ingredients = serializers.SerializerMethodField()
+    cooking_method = serializers.SerializerMethodField()
+
     class Meta:
         model = Recipe
         fields = '__all__'
+
+    def get_cuisine(self, obj):
+        return [c.strip() for c in obj.cuisine.split(',')]
+
+    def get_ingredients(self, obj):
+        return [i.strip() for i in obj.ingredients.split(',')]
+
+    def get_cooking_method(self, obj):
+        return [m.strip() for m in obj.cooking_method.split(',')]
